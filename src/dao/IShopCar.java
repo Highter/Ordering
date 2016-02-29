f3c1666b2fbddce4057f4ff14328ae30d0cb22d1
Higher <yjwsmileatlife@126.com>
2016-02-29 11:17:02 +0800

    test

--- src/dao/IShopCar.java
+++ src/dao/IShopCar.java
@@ -9,5 +9,6 @@ public interface IShopCar {
 	public  long addShopCar(ShoppingCarDateBase shopcar);
 	public List<ShoppingCar> getShopCars(int custom_id);
 	public boolean deleteShopCar(int custom_id);
+	
 
 }
